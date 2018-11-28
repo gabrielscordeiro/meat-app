e26974ca19f637f24e988bf1ca33aef72fa3dcee
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2018-11-28 13:18:39 -0200

    Commit inicial

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getTitleText() {
+    return element(by.css('meat-root h1')).getText();
+  }
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meat-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  content = 'Welcome do Meat App!'
+}
